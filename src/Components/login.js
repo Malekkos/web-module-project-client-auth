891b0dd2031d10a0383567ca233a01b8d3bd0fd8
Christian Anderson <tehninjawaffle@gmail.com>
2023-10-22 07:55:03 -0700

    Did some more styling, Added onChange and onSubmit to my component and posted the resulting changes in order to get my token. Token is then taken and inserted into localStorage

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-
-
+const initialData = {
+  username: "",
+  password: ""
+}
 
 const Login = () => {
-
-
+  const [data, setData] = useState(initialData)
+  console.log(data)
+  const onChange = (event) => {
+    setData({...data, [event.target.name] :event.target.value})
+  }
+  const onSubmit = (event) => {
+    event.preventDefault()
+    axios.post("http://localhost:9000/api/login", data)
+    .then(res => {
+      console.log(res.data)
+      localStorage.setItem("token", res.data.token)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
   return (
-    <div>
-      <div>
-        <h1>Login</h1>
+    <form onSubmit={event => onSubmit(event)}>
+      <div className="loginWrapper">
+        <div>
+          <h1 className="titleLogin">Login</h1>
+        </div>
+        <div className="credentialsWrapper">
+          <label className="username">Username</label>
+          <input className="usernameInput" onChange={event => onChange(event)} name="username" value={data.username} type="text" />
+          <label className="password">Password</label>
+          <input className="passwordInput" onChange={event => onChange(event)} name="password" value={data.password} type="password" />
+          <div>
+           <button className="submitButton">Login</button>
+          </div>
+          </div>
       </div>
-      <div>
-        <label className="username">Username</label>
-        <input className="usernameInput" type="text" />
-        <label className="password">Password</label>
-        <input className="passwordInput" type="password" />
-        <button className="submitButton">SUBMIT</button>
-      </div>
-
-
-
-    </div>
+    </form>
   )
 }
 

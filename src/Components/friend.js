bfc2a2e75e3d3df3c2d264058e88e432e20435ca
Christian Anderson <tehninjawaffle@gmail.com>
2023-10-23 01:35:56 -0700

    Added a bunch of styling to my friend and friendList component

--- src/Components/friend.js
+++ src/Components/friend.js
@@ -5,11 +5,11 @@ import React from "react";
 const Friend = (props) => {
   // console.log(props.friends)
   return (
-    <div>
+    <div className="friendsWrapper">
       {props.friends.map(friend => {
         // {console.log(friend)}
         return (
-        <div>
+        <div className="friendDetailsWrapper">
           <h3 className="friend" id={friend.id}>{friend.name} - {friend.email}</h3>
           <h4 className="friendAge">{friend.name} is {friend.age} years old</h4>
         </div>

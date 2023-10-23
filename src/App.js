3660d406bd2d1e58b21785a4e946c197fb7b0a36
Christian Anderson <tehninjawaffle@gmail.com>
2023-10-23 01:40:38 -0700

    Updated some code to reflect a route change of 'friends' and not 'friendList' to comply with requiremnts

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Link, Routes , Route} from 'react-router-dom';
 import Login from './Components/login';
-import FriendList from './Components/friendList';
+import FriendList from './Components/friends';
 function App() {
   return (
     <div className="App">
@@ -12,14 +12,14 @@ function App() {
         </div>
         <div className="wrapper">
           <Link to="/login" className="login">Login</Link>
-          <Link to="/friendList" className="friendList">Friend List</Link>
+          <Link to="/friends" className="friendList">Friend List</Link>
           <Link to="/addFriend" className="addFriend">Add Friend</Link>
           <Link to="/logout" className="logout">Logout</Link>
         </div>
       </nav>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/friendList" element={<FriendList />}/>
+        <Route path="/friends" element={<FriendList />}/>
         <Route path="/addFriend"/>
       </Routes>
     </div>
